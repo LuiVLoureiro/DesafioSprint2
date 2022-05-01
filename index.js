71116ef66c5ddef9a00cfd29f2ef024866d3ba41
LuiVLoureiro <luivloureiro@gmail.com>
2022-05-01 04:38:16 -0300

    Instalando Nodemon + Criando Primeira Rota com o método POST
    This reverts commit 1cad5b386e62e543eea9a141e97d83898fc900e6.

--- index.js
+++ index.js
@@ -6,6 +6,3 @@ const app = express()
 
 //Chamada da porta 3000 para subir servidor
 app.listen(3000, () => console.log('Servidor Operando com Sucesso'))
-
-
-app.post('/api/v1/user', (req, res) => res.send('Você está na rota de cadastro'))
