de2198d82a98ddbecc2cee5a26a6d472fbd1c8e9
LuiVLoureiro <luivloureiro@gmail.com>
2022-05-01 04:50:25 -0300

    Criando Método GET para reconhecimento

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ const app = express()
 //Chamada da porta 3000 para subir servidor
 app.listen(3000, () => console.log('Servidor Operando com Sucesso'))
 
+//Enviar Mensagem usando método GET
+app.get('/api/v1/user', (req, res) => {
+    res.send('Você está na rota de cadastro')
+})
 
-app.post('/api/v1/user', (req, res) => res.send('Você está na rota de cadastro'))
+//Enviar Dados ao Servidor usando método POST
+app.post('/api/v1/user', (req, res) => {
+    res.send('Você está na rota de cadastro')
+})
