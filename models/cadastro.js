0fd18b38ad8f313cb6d858d198c12115fee64de5
LuiVLoureiro <luivloureiro@gmail.com>
2022-05-02 08:24:16 -0300

    Adicionando Preferências e Atribuindo Erros

--- models/cadastro.js
+++ models/cadastro.js
@@ -6,13 +6,40 @@ class Cadastro {
     adiciona(cadastro, res) {
         const sql = 'INSERT INTO cadastro SET ?'
 
-        connection.query(sql, cadastro, (erro, results) => {
-            if (erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(results)
+        //Validar superficialmente o CPF atráves dos 11 dígitos 
+        const cpfValidation = cadastro.cpf.length < 11
+
+        //Validar senha a partir dos 6 dígitos
+        const passwordValidation = cadastro.password.length >= 6
+
+        //Campo de Validação
+        const validationSide = [{
+                cpf: 'cpf',
+                validation: cpfValidation,
+                alert: 'CPF deve conter 11 Dígitos'
+            },
+            {
+                password: 'password',
+                validation: passwordValidation,
+                alert: 'Senha deve conter no mínimo 6 dígitos'
             }
-        })
+        ]
+
+        //Const´s para discernir se haverá erros ou não
+        const erros = validationSide.filter(side => !side.true)
+        const errorFrequency = erros.length
+
+        if (errorFrequency) {
+            res.status(400).json(erros)
+        } else {
+            connection.query(sql, cadastro, (erro, results) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(results)
+                }
+            })
+        }
     }
 }
 
